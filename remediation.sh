c6f2fa2759c03c13c2973813f126d91e4558c736
Amir Sabahi <amir.sabahi@gmail.com>
2023-06-12 16:47:41 +0330

    Update remediation.sh

--- remediation.sh
+++ remediation.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Not a complete list of remediation.
+# NOTE: Not a complete list of remediation.
 
-# Check if the required command-line arguments are provided
-if [[ $# -lt 2 ]]; then
-    echo "Usage: $0 <db_user> <db_password>"
-    exit 1
-fi
+read -p "Enter MariaDB Username: " username
+read -s -p "Enter MariaDB Password: " password
+echo
+read -p "Enter MariaDB Host (default: 127.0.0.1): " host
+host=${host:-127.0.0.1}
+read -p "Enter MariaDB Port (default: 3306): " port
+port=${port:-3306}
 
 # Set permissions and ownership for /var/lib/mysql
 chmod 750 /var/lib/mysql
@@ -31,7 +33,8 @@ ln -s /dev/null $HOME/.mysql_history
 mariadb --local-infile=0 --load-data-local-dir=/my/local/data
 
 # Apply data-at-rest
-mkdir -p /etc/mysql/encryption && (echo -n "1;" ; openssl rand -hex 32) | tee -a /etc/mysql/encryption/keyfile
+mkdir -p /etc/mysql/encryption 
+echo -n "1;" ; openssl rand -hex 32 | tee -a /etc/mysql/encryption/keyfile
 openssl rand -hex 128 | tee -a /etc/mysql/encryption/keyfile.key
 openssl enc -aes-256-cbc -md sha1 \
 -pass file:/etc/mysql/encryption/keyfile.key \
@@ -40,36 +43,33 @@ openssl enc -aes-256-cbc -md sha1 \
 rm /etc/mysql/encryption/keyfile
 chown mysql:mysql -R /etc/mysql/encryption
 chmod 640 /etc/mysql/encryption/keyfile*
-
+echo -e "\033[0;33m Set ssl_cert ssl_key and ssl_ca for client and server. \033[0m"
 
 # Install mariadb-plugin-cracklib-password-check
 apt update
 apt-get --assume-yes install mariadb-plugin-cracklib-password-check
 
 # Run MariaDB Commands
-# Get the command-line arguments
-DB_USER="$1"
-DB_PASSWORD="$2"
+DB_USER=username
+DB_PASSWORD=password
 
 # Run the SQL commands
-mysql -u $DB_USER -p$DB_PASSWORD -e "SET GLOBAL slow_query_log = 'OFF';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "SET GLOBAL general_log = 'OFF';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "SET GLOBAL sql_mode = 'STRICT_ALL_TABLES,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "INSTALL SONAME 'simple_password_check';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "INSTALL SONAME 'cracklib_password_check';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "INSTALL SONAME 'server_audit';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "SET GLOBAL sql_mode ='STRICT_ALL_TABLES,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "alter user 'root'@'localhost' identified via 'unix_socket'; set password for 'mysql'@'localhost' = 'invalid'; set password for 'mariadb.sys'@'localhost' = 'invalid';"
-echo -e "\033[0;33m Run the following for each user manually: alter user 'laravel'@'localhost' identified via 'unix_socket'; \033[0m"
+mysql -u"$username" -p"$password" -h"$host" -P"$port" -e "SET GLOBAL slow_query_log = 'OFF';"
+mysql -u"$username" -p"$password" -h"$host" -P"$port" -e "SET GLOBAL general_log = 'OFF';"
+mysql -u"$username" -p"$password" -h"$host" -P"$port" -e "SET GLOBAL sql_mode = 'STRICT_ALL_TABLES,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';"
+mysql -u"$username" -p"$password" -h"$host" -P"$port" -e "INSTALL SONAME 'simple_password_check';"
+mysql -u"$username" -p"$password" -h"$host" -P"$port" -e "INSTALL SONAME 'cracklib_password_check';"
+mysql -u"$username" -p"$password" -h"$host" -P"$port"-e "INSTALL SONAME 'server_audit';"
+mysql -u"$username" -p"$password" -h"$host" -P"$port"-e "SET GLOBAL sql_mode ='STRICT_ALL_TABLES,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';"
+mysql -u"$username" -p"$password" -h"$host" -P"$port" -e "alter user 'root'@'localhost' identified via 'unix_socket'; set password for 'mysql'@'localhost' = 'invalid'; set password for 'mariadb.sys'@'localhost' = 'invalid';"
+echo -e "\033[0;33m Run the following for each user manually: ALTER USER 'laravel'@'localhost' identified via 'unix_socket'; \033[0m"
 echo -e "\033[0;33m Run the following for each user manually: ALTER USER 'user_name'@'localhost' REQUIRE SSL; \033[0m"
-
 
 chmod 660 /root/server_audit.log
 chown mysql:mysql /root/server_audit.log
 
-
 usermod -s /bin/false mysql
 
 echo -e "\033[0;33m Ensure No Users Have Wildcard Hostnames. Use ALTER USER \033[0m"
-echo -e "\033[0;33m Now you can enable encyption for any table in the desired database. (ALTER TABLE table_name ENCRYPTED=YES ENCRYPTION_KEY_ID=1;) \033[0m"
+echo -e "\033[0;33m Now you can enable encyption for any table in the desired database. Run this for each table : ALTER TABLE table_name ENCRYPTED=YES ENCRYPTION_KEY_ID=1; \033[0m"
 echo "Done!"
